b7c3f297691787a74ab52d4438fa85a437bd1428
Paul Applegate <paul@theapplegates.com>
2022-03-03 12:54:47 +0000

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
         name: `content`,
       },
     },
+    'gatsby-plugin-netlify',
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
